46602afbdecb4d5cac6ad0ecf2919c7485180ca6
Finlay Plumb <finlay.plumb@bnc.ox.ac.uk>
2025-10-01 15:13:07 +0100

    Fixed unit test

--- src/tests/test_unit_at.py
+++ src/tests/test_unit_at.py
@@ -8,7 +8,7 @@ class TestAtoms(unittest.TestCase):
 
     def test_basic_conversion(self):
         self.assertEqual(at.to_si((6.022 * (10 ** 23))), 1)
-        self.assertAlmostEqual(mol.to_unit(1, at), (6.022 * (10 ** 23)), 8)
+        self.assertAlmostEqual(mol.to_unit(1, at), (6.022 * (10 ** 23)), delta=70000000)
 
 
 if __name__ == '__main__':
